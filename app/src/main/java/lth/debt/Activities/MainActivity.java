0e2dae40f5f95cbf03bd81da950f384eb3aaab67
unknown <calle.danell@gmail.com>
2015-06-30 13:06:48 +0200

    color change navigation and restructure

--- app/src/main/java/lth/debt/Activities/MainActivity.java
+++ app/src/main/java/lth/debt/Activities/MainActivity.java
@@ -1,12 +1,14 @@
 package lth.debt.Activities;
 
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
+import java.util.ArrayList;
 import lth.debt.Adapters.SectionPagerAdapter;
 import lth.debt.R;
 
@@ -14,23 +16,77 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private Toolbar toolbar;
     private ViewPager viewPager;
+    private SectionPagerAdapter sectionPagerAdapter;
+    private ArrayList<Integer> imageResId;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initVars();
+        setupTabLayout();
+        setUpViewPager();
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                updateIconColor(getResources().getColor(R.color.black), tab);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                updateIconColor(getResources().getColor(R.color.gray), tab);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                updateIconColor(getResources().getColor(R.color.black), tab);
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (sectionPagerAdapter.back()) {
+            super.onBackPressed();
+        }
+    }
+
+
+    private void initVars() {
+        imageResId = new ArrayList<>();
+        imageResId.add(R.drawable.ic_credit_card);
+        imageResId.add(R.drawable.ic_add_group);
+        imageResId.add(R.drawable.ic_settings_menu);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         viewPager = (ViewPager) findViewById(R.id.pager);
-
+        sectionPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager());
         if (toolbar != null) {
             setSupportActionBar(toolbar);
             toolbar.setNavigationIcon(R.mipmap.toolbar_icon);
         }
+    }
 
-        viewPager.setAdapter(new SectionPagerAdapter(this,getSupportFragmentManager()));
-        tabLayout.setupWithViewPager(viewPager);
+    private void updateIconColor(int color, TabLayout.Tab tab) {
+        Drawable img = getDrawable(imageResId.get(tab.getPosition()));
+        PorterDuff.Mode mMode = PorterDuff.Mode.SRC_ATOP;
+        img.setColorFilter(color, mMode);
+    }
 
+    private void setupTabLayout() {
+        tabLayout.addTab(tabLayout.newTab().setIcon(imageResId.get(0)));
+        tabLayout.addTab(tabLayout.newTab().setIcon(imageResId.get(1)));
+        tabLayout.addTab(tabLayout.newTab().setIcon(imageResId.get(2)));
+        updateIconColor(getResources().getColor(R.color.black), tabLayout.getTabAt(0));
+        updateIconColor(getResources().getColor(R.color.gray), tabLayout.getTabAt(1));
+        updateIconColor(getResources().getColor(R.color.gray), tabLayout.getTabAt(2));
+    }
+
+    private void setUpViewPager() {
+        viewPager.setAdapter(sectionPagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 }
--- app/src/main/java/lth/debt/Adapters/SectionPagerAdapter.java
+++ app/src/main/java/lth/debt/Adapters/SectionPagerAdapter.java
@@ -1,41 +1,20 @@
 package lth.debt.Adapters;
 
-import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ImageSpan;
-import android.util.Log;
 
 import lth.debt.Fragments.ActionFragment;
 import lth.debt.Fragments.DebtFragment;
-import lth.debt.R;
 import lth.debt.Fragments.SettingsFragment;
 
 /**
  * Created by Carl on 2015-06-29.
  */
 public class SectionPagerAdapter extends FragmentPagerAdapter {
-    private Context context;
-    private int[] imageResId = {
-            R.drawable.ic_credit_card,
-            R.drawable.ic_add_group,
-            R.drawable.ic_settings_menu
-    };
 
-    private String[] titles = {
-            "Actions",
-            "Debt",
-            "Profile"
-    };
-
-    public SectionPagerAdapter(Context context, FragmentManager fm) {
+    public SectionPagerAdapter(FragmentManager fm) {
         super(fm);
-        this.context=context;
-
     }
 
     @Override
@@ -48,7 +27,8 @@ public class SectionPagerAdapter extends FragmentPagerAdapter {
             case 2:
                 return new SettingsFragment();
             default:
-                return new ActionFragment();
+
+                return null;
         }
     }
 
@@ -57,14 +37,8 @@ public class SectionPagerAdapter extends FragmentPagerAdapter {
         return 3;
     }
 
-    @Override
-    public CharSequence getPageTitle(int position) {
-        Drawable image = context.getDrawable(imageResId[position]);
-        image.setBounds(0, 0, image.getIntrinsicWidth()+44, image.getIntrinsicHeight()+44);
-        Log.d("h", String.valueOf(image.getIntrinsicHeight()));
-        SpannableString sb = new SpannableString(" ");
-        ImageSpan imageSpan = new ImageSpan(image);
-        sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        return sb;
-    }
+    public boolean back() {
+        return true;
+        }
+
 }
